378290ee76f0a93ff259f30dc15f341162b59f5a
Jan Wilmake <jan@wilmake.com>
2025-01-27 21:03:46 +0100

    improvements

--- set-secrets.sh
+++
@@ -1,6 +0,0 @@
-# Export local vars to GitHub secrets
-cat .dev.vars | while read line; do
-  KEY=$(echo $line | cut -d'=' -f1)
-  VALUE=$(echo $line | cut -d'=' -f2)
-  gh secret set "$KEY" -b"$VALUE"
-done
